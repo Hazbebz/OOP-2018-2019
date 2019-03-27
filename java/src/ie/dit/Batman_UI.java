47aefce027292128ca1036833d2f14fb74159401
Your Name <you@example.com>
2019-03-27 11:54:22 +0000

    Assignment commit

---
+++ java/src/ie/dit/Batman_UI.java
@@ -0,0 +1,19 @@
+//Controlling class that allows for drawing 
+package ie.dit;
+
+import processing.core.PApplet;
+
+public class Batman_UI extends PApplet
+{
+
+    public void settings()
+    {
+        size(500,1000);
+    }
+
+    public void setup()
+    {
+        
+    }
+    
+}
