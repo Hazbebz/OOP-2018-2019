c24c032b1cc39b781509b9ce4f9dbd8c5d732cbb
Your Name <you@example.com>
2019-04-01 11:37:03 +0100

    bug fix

--- java/src/ie/dit/Bullet.java
+++ java/src/ie/dit/Bullet.java
@@ -7,42 +7,7 @@ public class Bullet extends GameObject
 {
     public Bullet(YASC yasc, float x, float y, float rotation)
     {
-<<<<<<< HEAD
-        this.yasc = yasc;
-        pos = new PVector(x, y);
-        forward = new PVector(0, -1);
-        this.rotation = rotation; 
-        speed = 5;
-
-        if(pos.x<0)
-        {
-            pos.x =yasc.width;
-        }
-
-        if(pos.x>yasc.width)
-        {
-            pos.x = 0;
-        }
-
-        if(pos.y<0)
-        {
-            pos.y = yasc.height;
-        }
-
-        if (pos.y > yasc.height)
-        {
-            pos.y=0;
-        }
-
-        alive += yasc.timeDelta;
-        if(alive >= 5.0)
-        {
-            yasc.bullets.remove(this);
-        }
-
-=======
         super(yasc, x, y, rotation, 5);
->>>>>>> d7d78f9b01f7865682ab1f94e2c03fdc6def4a4e
     }  
 
     public void render()
@@ -72,40 +37,6 @@ public class Bullet extends GameObject
 
         // pos += forward * speed
         pos.add(PVector.mult(forward, speed));
-<<<<<<< HEAD
-
-
-    }
-
-    /**
-     * @return the pos
-     */
-    public PVector getPos() {
-        return pos;
-    }
-
-    /**
-     * @param pos the pos to set
-     */
-    public void setPos(PVector pos) {
-        this.pos = pos;
-    }
-
-    /**
-     * @return the forward
-     */
-    public PVector getForward() {
-        return forward;
-    }
-
-    /**
-     * @param forward the forward to set
-     */
-    public void setForward(PVector forward) {
-        this.forward = forward;
-    }
-=======
->>>>>>> d7d78f9b01f7865682ab1f94e2c03fdc6def4a4e
 
         if (pos.x < 0)
         {
