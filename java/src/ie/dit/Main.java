70e9cc2fe5b4828de3f5e3d6e1cacb50f34026a9
Your Name <you@example.com>
2019-04-30 22:15:28 +0100

    deleting unused files

--- java/src/ie/dit/Main.java
+++ java/src/ie/dit/Main.java
@@ -1,63 +1,9 @@
 package ie.dit;
 
-import processing.core.PVector;
 
 public class Main
 {	
 
-	public void helloProcessing()
-	{
-		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new HelloProcessing());
-	}
-	
-	public void bugZap()
-	{
-		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new BugZap());
-	}
-
-	public void loops()
-	{
-		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new Loops());
-		
-	}
-	
-	public void arrays()
-	{
-		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new Arrays());
-		
-	}
-	
-	public void starMap()
-	{
-		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new StarMap());
-		
-	}
-	
-	public void cafeRubis()
-	{
-		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new CafeRubis());
-		
-	}
-
-	public void yasc()
-	{
-		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new YASC());
-		
-	}
-
-	public void audio1()
-	{
-		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new Audio1());
-		
-	}
 
 	public void Batman()
 	{
@@ -71,4 +17,5 @@ public class Main
 			Main main = new Main();
 			main.Batman();
 	}
+	
 }
